b0607156b58659428d53d74795a60596b51fe0f7
abha <abhabhagat@itgeeks.com>
2025-10-03 16:54:15 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
-const header_menu_items=document.getElementById("header-menu-items");
-// header_menu_items.addEventListener((),)
+const SearchBtn=document.getElementById("search-Btn");
+const inputBox=document.getElementById("search-content-wrap")
+const headerOutterWrap=document.getElementById("header-outter-wrap") 
+const crossBtn=document.getElementById("cross-input-Btn")
+
+
+const cartSlidBtn=document.getElementById("cart-box")
+const cartSlider=document.getElementById("drawer-inner-empty")
+const SliderCrossBtn=document.getElementById("cart-cross-Btn")
+
+
+SearchBtn.addEventListener("click",(event)=>{
+    
+    event.preventDefault()
+
+    inputBox.classList.remove("hidden")
+    inputBox.classList.add("display")
+    headerOutterWrap.classList.add("hidden")
+    // console.log("working")
+})
+crossBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    inputBox.classList.add("hidden")
+    inputBox.classList.remove("display")
+    headerOutterWrap.classList.remove("hidden")
+})
+
+cartSlidBtn.addEventListener("click",(event)=>{
+    event.preventDefault()
+    cartSlider.classList.add("slid");
+
+})
+SliderCrossBtn.addEventListener("click",(event)=>{
+    event.preventDefault()
+    cartSlider.classList.remove("slid")
+})
