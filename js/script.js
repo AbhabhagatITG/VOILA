41ba73e544749188ee7272ab0bfec62d8efe0e38
abha <abhabhagat@itgeeks.com>
2025-10-04 17:17:39 +0530

    second last commit

--- js/script.js
+++ js/script.js
@@ -2,35 +2,29 @@ const SearchBtn=document.getElementById("search-Btn");
 const inputBox=document.getElementById("search-content-wrap")
 const headerOutterWrap=document.getElementById("header-outter-wrap") 
 const crossBtn=document.getElementById("cross-input-Btn")
-
-
 const cartSlidBtn=document.getElementById("cart-box")
 const cartSlider=document.getElementById("drawer-inner-empty")
 const SliderCrossBtn=document.getElementById("cart-cross-Btn")
-
-
-SearchBtn.addEventListener("click",(event)=>{
-    
-    event.preventDefault()
-
+const MegaMenuSlider=document.getElementById("mega-menu-slider")
+const burger=document.getElementById("burger")
+SearchBtn.addEventListener("click",()=>{  
     inputBox.classList.remove("hidden")
     inputBox.classList.add("display")
     headerOutterWrap.classList.add("hidden")
-    // console.log("working")
 })
-crossBtn.addEventListener("click",(event)=>{
-    event.preventDefault();
+crossBtn.addEventListener("click",()=>{
     inputBox.classList.add("hidden")
     inputBox.classList.remove("display")
     headerOutterWrap.classList.remove("hidden")
 })
-
-cartSlidBtn.addEventListener("click",(event)=>{
-    event.preventDefault()
+cartSlidBtn.addEventListener("click",()=>{
     cartSlider.classList.add("slid");
-
 })
-SliderCrossBtn.addEventListener("click",(event)=>{
-    event.preventDefault()
+SliderCrossBtn.addEventListener("click",()=>{
     cartSlider.classList.remove("slid")
 })
+function toggleMenu(){
+MegaMenuSlider.classList.toggle("hidden")
+MegaMenuSlider.classList.toggle("display")
+burger.classList.toggle("display")
+}
